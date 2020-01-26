e73ffa9eee2adc3aa6f5c42ce4e3b5115a5c462a
frankkode <masasimpafra@gmail.com>
2020-01-26 22:03:37 +0100

    uncoment code

--- sawasawa_market/settings.py
+++ sawasawa_market/settings.py
@@ -107,16 +107,6 @@ else:
 
 
 
-#DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-
-#else:
-    #print('Database URL not found. Using SQLite instead')
-    #DATABASES = {
-       # 'default': {
-        #    'ENGINE': 'django.db.backends.sqlite3',
-         #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #}
-   # }
 
 
 # Password validation
