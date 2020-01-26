01073ba21502bfe0229ca6bce8e8a0719845cfe4
frankkode <masasimpafra@gmail.com>
2020-01-26 19:00:50 +0100

    travis

--- blog/tests.py
+++ blog/tests.py
@@ -5,11 +5,4 @@ from .models import Post
 
 # Create your tests here.
 class PostTests(TestCase):
-    """
-    Here we'll define the tests that we'll run against our
-    Post model
-    """
-
-    def test_str(self):
-        test_name = Post(name='A post')
-        self.assertEqual(str(test_name), 'A post')
+    
